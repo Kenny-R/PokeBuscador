a6dbdba700e017ab33a43678ef2c5967058e3343
Ana <73360276+anna-ayn@users.noreply.github.com>
2023-04-13 17:52:31 -0400

    Create busqueda.h

---
+++ busqueda.h
@@ -0,0 +1,12 @@
+
+#ifndef BUSQUEDA_LIB
+    #define BUSQUEDA_LIB
+    /* struct del busqueda */
+    typedef struct
+    {
+        char *region;
+        char *especie;
+        char *tipo;
+        char *nombre;
+    } busqueda;
+#endif
